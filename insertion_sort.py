988102d79a51f27a09c6bdde71511a4d1b7d58ad
Gunakar-Maddi <gmaddi@uncc.edu>
2026-01-18 18:44:42 -0700

    Add comments

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,14 +2,20 @@ from typing import List
 
 class Solution:    
     def insertion_sort(self, arr: List[int]) -> List[int]:
-        for i in range(1, len(arr)):
-            key = arr[i]
-            j = i - 1
+        """
+        Sort the input array in monotonically decreasing order 
+        using Insertion Sort Algorithm
+        """
 
-            while j >= 0 and arr[j] < key:
+        # Traverse from the second element to the end of the array
+        for i in range(1, len(arr)):
+            key = arr[i] # store the current element as key
+            j = i - 1 # j points to the last index of the sorted part
+
+            while j >= 0 and arr[j] < key: # shift elements by one position to the right if they are smaller
                 arr[j + 1] = arr[j]
                 j -= 1
-            arr[j + 1] = key
+            arr[j + 1] = key #insert key at correct position
 
         return arr
     
